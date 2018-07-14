1080cab70922274ca16b2bc48b46e5006541bee7
Tal Oyar <tal.oyar@imperva.com>
2018-07-14 19:41:09 +0300

    login modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { RouterModule, Routes } from '@angular/router';
 import {FormsModule}from '@angular/forms';
+import { UserService } from './shared/user.service';
+import { HttpClientModule } from '../../node_modules/@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {ToastrModule} from 'ngx-toastr';
+import { UserComponent } from './user/user.component'; 
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { LoginComponent } from './user/login/login.component';
+
 
 const appRoutes: Routes = [
   { path: 'Home', component: HomeComponent },
@@ -19,18 +25,21 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    SignUpComponent
+    LoginComponent, 
+    SignUpComponent, UserComponent
   ],
   imports: [
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     )
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
