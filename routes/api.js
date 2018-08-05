a4d3550ff492acd26af8624ce7f8c7696f73c9a2
Omer Avrech <omera@ceragon.com>
2018-08-05 21:55:37 +0300

    finish login

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ var db        = require('../core/db');
 
 var router    = express.Router();
 
-router.post('/register', function (req, res, next) {
+router.post('/register', function (req, res) {
     try {
         let params = req.body;
         if (!params.email)    throw Error('ERR_INVALID_INPUT');
@@ -20,7 +20,7 @@ router.post('/register', function (req, res, next) {
         if (!validator.isByteLength(params.last, {min: 2, max: 30}))  throw new Error('ERR_INVALID_LAST');
 
         params["salt"] = uniqid();
-        var hash = crypto.createHash('sha256');
+        let hash = crypto.createHash('sha256');
         hash.update(params["salt"] + params["password"]);
         params["password"] = hash.digest('hex');
 
@@ -42,7 +42,27 @@ router.post('/register', function (req, res, next) {
     }
 });
 
-router.post('/login', function (req, res, next) {
+router.post('/login', function (req, res) {
+    try{
+    if (!req.body.email)                      throw Error('ERR_INVALID_INPUT');
+    if (!validator.isEmail(req.body.email))   throw new Error('ERR_INVAILD_EMAIL');
+    if (!req.body.password)                   throw Error('ERR_INVALID_INPUT');
+
+    db.select('users', {"email": req.body.email})
+        .then((userData => {
+            let hash = crypto.createHash('sha256');
+            hash.update(userData[0]["salt"] + req.body.password);
+            if (hash.digest('hex') === userData[0]["password"]) {
+                let token = jwt.sign({ _id: userData[0]._id }, require('../configuration').authentication.secret, {
+                    expiresIn: 86400 // expires in 24 hours
+                });
+                res.json({"code": 200, "status": "Success", "data": { "token" : token }});
+            }
+            else { res.status(401).json({"code": 403, "status": "error", "data": "User or password not match"}) }
+        })).catch (err => res.status(401).json({"code": 401, "status": "error", "data": "User not exist"}));
+    } catch (err) {
+        res.status(400).json({"code": 400, "status": "error", "data": err});
+    }
 });
 
 module.exports  = router;
