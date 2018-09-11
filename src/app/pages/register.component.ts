f5f03000c7a7f32aa4e18e5c062a5ad1ba7ddf5d
May <maymelodi@gmail.com>
2018-09-11 22:00:09 +0300

    Added register

--- src/app/pages/register.component.ts
+++ src/app/pages/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 
 @Component({
@@ -8,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
+    this.model = {
+      firstname: "",
+      lastname: "",
+      userName:"",
+      email: "",
+      password: ""
+    };
+    this.submitted = false;
   }
 
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-zA-Z]{2,4}$";
+  namePattern = "[a-zA-Z]";
+
+  model: RegistrationDetails;
+  submitted: boolean;
+
+  onSubmit(form: NgForm) {
+    this.submitted = true;
+    console.log(JSON.stringify(this.model));
+  }
+}
+  class RegistrationDetails {
+    public firstname: string;
+    public lastname: string;
+    public userName: string;
+    public email: string;
+    public password: string;
+  
+    constructor() { 
+        this.firstname = null;
+        this.lastname = null;
+        this.userName = null;
+        this.email = null;
+        this.password = null;
+     }
 }
